5764c3a5a3a10243bdc9c68b2265e32e11a27e7a
7Nirmal <ra.nirmalkumar96@gmail.com>
2022-06-21 13:12:27 +0530

    formik

--- src/BasicForm.js
+++ src/BasicForm.js
@@ -2,25 +2,17 @@ import {useFormik} from "formik";
 
 export function BasicForm() {
   const formik = useFormik({
-    intialValues : {email:"google", password:"abc"},
-    onSubmit:(values) =>{
-      console.log("onSubmit",values );
-    },
-  })
-  return (
-    <form onSubmit={formik.handleSubmit}>
-            <input type="email" 
-            placeholder="Enter email" 
-             name="email" 
-             value={formik.values.email} 
-             onChange={formik.handleChange}/>
+    initialValues:{email:"google" , password:""},
+    onSubmit:(values) => {
+      console.log("onSubmit ",values);
+    } 
+  });
 
-      <input  type="password" 
-      placeholder="Enter password"  
-      name="password" 
-      value={formik.values.password} 
-      onChange={formik.handleChange}/>
-      <button type="submit">Submit</button>
-    </form>
+  return (
+<form onSubmit={formik.handleSubmit}>
+  <input type="email" name="email" value={formik.values.email} placeholder="Enter email" onChange={formik.handleChange}/>
+  <input type="password" name="password"value={formik.values.password} placeholder="Enter password" onChange={formik.handleChange}/>
+  <button type="submit">submit</button>
+</form>
   );
 }
